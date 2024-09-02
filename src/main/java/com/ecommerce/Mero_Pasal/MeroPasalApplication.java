882c90ce089b2ddc6f83231dd2c9819c51886503
Your Name <your.email@example.com>
2024-09-02 18:19:30 -0400

    first commit

---
+++ src/main/java/com/ecommerce/Mero_Pasal/MeroPasalApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Mero_Pasal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeroPasalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeroPasalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/Mero_Pasal/MeroPasalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Mero_Pasal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeroPasalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
